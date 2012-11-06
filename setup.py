1407904a7d0dec5eb14cf1d4f39a9d4cd1cb3078
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2012-11-06 09:00:46 -0700

    Use setuptools.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+packages = ['pyutils']
+
+setup(name='pyutils',
+      version='0.1',
+      author='Alejandro Weinstein',
+      author_email='alejandro.weinstein@gmail.com',
+      packages=packages,
+      description='Some utility functions',
+      long_description=open('README.rst').read(),
+      )
